0b97f52af5e71b2c397245a40e5fe39a6b23a0bf
Vaida Kareivaite <vkkareivaite@gmail.com>
2024-05-27 16:26:41 +0100

    bookmarks controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root to: 'lists#index'
-  resources :lists, only: %i[show new create]
-
+  resources :lists, only: %i[show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: [:destroy]
   # Defines the root path route ("/")
   # root "posts#index"
 end
